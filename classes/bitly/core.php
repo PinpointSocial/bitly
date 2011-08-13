ffaf67e29b91c272941e58c253fc77052b17e333
Alex Tucker <tucker.al@gmail.com>
2011-08-13 16:10:37 -0400

    Constructor and tests greened

--- classes/bitly/core.php
+++ classes/bitly/core.php
@@ -11,15 +11,28 @@ abstract class Bitly_Core {
 	 // Get from Kohana config
 	 $config = Kohana::config('bitly')->$config_type;
       }
-
       // Check the config
       if(empty($config['login']) || empty($config['api_key'])){
 	 throw new Bitly_Exception('Invalid Configuration Argument(s)');  
       }
+      return new Bitly($config['login'], $config['api_key']);
    }
 
    public function __construct($login, $api_key) {
+      $this->login = trim($login);
+      $this->api_key = trim($api_key);
+   }
 
+   public function shorten($url) {
+      
+   }
+
+   public function get_login() {
+      return $this->login;
+   }
+   
+   public function get_api_key() {
+      return $this->api_key;
    }
 
 }
--- tests/InstanceTest.php
+++ tests/InstanceTest.php
@@ -1,22 +1,24 @@
 <?php
 
 class InstanceTest extends PHPUnit_Framework_TestCase {
-   
+
    public function testKohanaConfigPass() {
-      $bitly = Bitly::instance(); 
+      $bitly = Bitly::instance();
    }
 
    public function testManualConfigPass() {
       $config = array(
-	 'login' => 'login',
-	 'api_key' => 'api_key',
-      );
+	    'login' => 'login',
+	    'api_key' => 'api_key',
+	    );
       $bitly = Bitly::instance($config);
+      $this->assertEquals('login', $bitly->get_login());
+      $this->assertEquals('api_key', $bitly->get_api_key());
    }
-   
+
    /**
-   *  @expectedException   Bitly_Exception
-   */
+    *  @expectedException   Bitly_Exception
+    */
    public function testManualConfigBlankLogin() {
       $config = array(
 	    'login' => '',
@@ -24,10 +26,10 @@ class InstanceTest extends PHPUnit_Framework_TestCase {
 	    );
       $bitly = Bitly::instance($config);
    }
-   
+
    /**
-   *  @expectedException   Bitly_Exception
-   */
+    *  @expectedException   Bitly_Exception
+    */
    public function testManualConfigNullLogin() {
       $config = array(
 	    'api_key' => 'api_key',
@@ -45,7 +47,7 @@ class InstanceTest extends PHPUnit_Framework_TestCase {
 	    );
       $bitly = Bitly::instance($config);
    }
-   
+
    /**
     *  @expectedException   Bitly_Exception
     */
